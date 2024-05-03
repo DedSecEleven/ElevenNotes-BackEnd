6e9506f04cca91b7170894eb2b6c6f0304dfe277
kemtch19 <kemtch19@gmail.com>
2024-05-03 14:16:17 -0500

    feat: nuevas funcionalidades al controlador notes

--- Controllers/NotesController.cs
+++ Controllers/NotesController.cs
@@ -30,5 +30,28 @@ namespace ElevenNotesBackEnd.Controllers{
       }
       return note;
     }
+    // Crear un usuario
+    [HttpPost]
+    public async Task<ActionResult<Note>> CreateNotes(Note note)
+    {
+      _Context.Notes.Add(note);
+      await _Context.SaveChangesAsync();
+      return CreatedAtAction("GetPerson", new { id = note.Id }, note);
+    }
+
+    // Eliminar un usuario
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteNote(int id)
+    {
+      var note = await _Context.Users.FindAsync(id);
+      System.Console.WriteLine(note);
+      if (note == null)
+      {
+        return NotFound();
+      }
+      _Context.Notes.Remove(note);
+      await _Context.SaveChangesAsync();
+      return NoContent();
+    }
   }
 }
